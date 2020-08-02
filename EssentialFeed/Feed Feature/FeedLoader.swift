c1da20eb973a64963e150aa538167c5784c8d4ff
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-08-02 16:05:12 -0500

    Makes `FeedLoader` protocol public

--- EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -9,6 +9,6 @@ import Foundation
 
 public typealias LoadFeedResult = Result<[FeedItem], Error>
 
-protocol FeedLoader {
+public protocol FeedLoader {
     func load(completion: @escaping (LoadFeedResult) -> Void)
 }
