7d3279f18eb963f28a5d489bcffe8f184e2572ea
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-08-09 17:25:41 -0500

    [TEST] Save command fails on cache deletion error.

--- EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
@@ -14,8 +14,9 @@ class LocalFeedLoader {
 		self.currentDate = currentDate
 	}
 	
-	func save(_ items: [FeedItem]) {
+	func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
 		store.deleteCachedFeed { [unowned self] error in
+			completion(error)
 			if error == nil {
 				self.store.insert(items, timestamp: self.currentDate())
 			}
@@ -64,7 +65,7 @@ class CacheFeedUseCase: XCTestCase {
 		let (sut, store) = makeSUT()
 		let items = [uniqueItem(), uniqueItem()]
 		
-		sut.save(items)
+		sut.save(items) { _ in }
 		
 		XCTAssertEqual(store.receivedMessages, [.deleteCacheFeed])
 	}
@@ -74,7 +75,7 @@ class CacheFeedUseCase: XCTestCase {
 		let items = [uniqueItem(), uniqueItem()]
 		let deletionError = anyNSError
 		
-		sut.save(items)
+		sut.save(items) { _ in }
 		store.completeDeletion(with: deletionError)
 		
 		XCTAssertEqual(store.receivedMessages, [.deleteCacheFeed])
@@ -85,10 +86,27 @@ class CacheFeedUseCase: XCTestCase {
 		let (sut, store) = makeSUT(currentDate: { timestamp })
 		let items = [uniqueItem(), uniqueItem()]
 		
-		sut.save(items)
+		sut.save(items) { _ in }
 		store.completeDeletionSuccessfully()
 		
 		XCTAssertEqual(store.receivedMessages, [.deleteCacheFeed, .insert(items, timestamp)])
+	}
+	
+	func test_save_failsOnDeletionError() {
+		let (sut, store) = makeSUT()
+		let items = [uniqueItem(), uniqueItem()]
+		let deletionError = anyNSError
+		let exp = expectation(description: "Wait for save completion")
+		
+		var receivedError: Error?
+		sut.save(items) { error in
+			receivedError = error
+			exp.fulfill()
+		}
+		store.completeDeletion(with: deletionError)
+		
+		wait(for: [exp], timeout: 1.0)
+		XCTAssertEqual(receivedError as NSError?, deletionError)
 	}
 	
 	// MARK: - Test Helpers
