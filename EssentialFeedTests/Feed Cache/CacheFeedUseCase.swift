02fd0f6acce024df87f84aa38108ee9e10f0afd8
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-08-09 18:12:01 -0500

    [REFACTOR] Inverts if logic to make code paths easier to follow.

--- EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
@@ -17,13 +17,13 @@ class LocalFeedLoader {
 	func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
 		store.deleteCachedFeed { [weak self] error in
 			guard let self = self else { return }
-			if error == nil {
+			if let cacheDeletionError = error {
+				completion(cacheDeletionError)
+			} else  {
 				self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
 					guard self != nil else { return }
 					completion(error)
 				}
-			} else {
-				completion(error)
 			}
 		}
 	}
