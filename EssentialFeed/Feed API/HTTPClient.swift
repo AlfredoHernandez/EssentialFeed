ef3fe63d9b7b11cc6a163190ee644f4dfa29f483
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-08-12 16:45:59 -0500

    [DOCS]  Clients are responsible to dispatch to appropriate threads, if needed.
    - Adds comments to document that completion handlers in any `HTTPClient` implementation can be invoked in any thread.

--- EssentialFeed/Feed API/HTTPClient.swift
+++ EssentialFeed/Feed API/HTTPClient.swift
@@ -7,5 +7,7 @@ import Foundation
 public typealias HTTPClientResult = Result<(Data, HTTPURLResponse), Error>
 
 public protocol HTTPClient {
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
