8503219ee2afdb1983b9d3026465db94c2f02473
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-08-16 15:36:20 -0500

    [FEAT] Load persistent container stores upon `CoreDataFeedStore` initialization

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -6,7 +6,11 @@ import CoreData
 import Foundation
 
 public final class CoreDataFeedStore: FeedStore {
-    public init() {}
+    let container: NSPersistentContainer
+    
+    public init(bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+    }
 
     public func retrieve(completion: @escaping RetreivalCompletion) {
         completion(.empty)
@@ -15,6 +19,34 @@ public final class CoreDataFeedStore: FeedStore {
     public func insert(_: [LocalFeedImage], timestamp _: Date, completion _: @escaping InsertionCompletion) {}
 
     public func deleteCachedFeed(completion _: @escaping DeletionCompletion) {}
+}
+
+private extension NSPersistentContainer {
+    enum LoadingError: Swift.Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Swift.Error)
+    }
+    
+    static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer {
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+            throw LoadingError.modelNotFound
+        }
+        
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        var loadError: Swift.Error?
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+        
+        return container
+    }
+}
+
+private extension NSManagedObjectModel {
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        return bundle
+            .url(forResource: name, withExtension: "momd")
+            .flatMap { NSManagedObjectModel(contentsOf: $0) }
+    }
 }
 
 private class ManagedCache: NSManagedObject {
