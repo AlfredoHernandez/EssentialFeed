8791bab2e599c091d719ca141720cccff7d11367
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-08-16 12:57:50 -0500

    [TEST] Adds empty core data feed store specs.

---
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,31 @@
+//
+//  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import XCTest
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {}
+
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {}
+
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {}
+
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {}
+
+    func test_insert_deliversNoErrorOnEmptyCache() {}
+
+    func test_insert_deliversNoErrorOnNonEmptyCache() {}
+
+    func test_insert_overridesPreviouslyInsertedCacheValues() {}
+
+    func test_delete_deliversNoErrorOnEmptyCache() {}
+
+    func test_delete_hasNoSideEffectsOnEmptyCache() {}
+
+    func test_delete_deliversNoErrorOnNonEmptyCache() {}
+
+    func test_delete_emptiesPreviouslyInsertedCache() {}
+
+    func test_storeSideEffects_runSerially() {}
+}
