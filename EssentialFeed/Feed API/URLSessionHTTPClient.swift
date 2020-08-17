2492a5c43aa7ea96a52306c2566ed50b76497518
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-08-16 19:38:58 -0500

    [RFT] Replaces custom `HTTPClientResult` enum
    With a nested typealias over the standard `Swift.Result`

--- EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -13,7 +13,7 @@ public class URLSessionHTTPClient: HTTPClient {
 
     private class UnexpectedValuesRepresentation: Error {}
 
-    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
         session.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -174,11 +174,11 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
     }
 
     private class HTTPClientSpy: HTTPClient {
-        var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
 
         var requestedURLs: [URL] { messages.map { $0.url } }
 
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
             messages.append((url, completion))
         }
 
