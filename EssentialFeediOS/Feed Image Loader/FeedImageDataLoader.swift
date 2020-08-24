90fac488b9bcee6273391495615e167fa93653f5
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-08-24 14:10:33 -0500

    Groups files in an MVC folder structure

---
+++ EssentialFeediOS/Feed Image Loader/FeedImageDataLoader.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
