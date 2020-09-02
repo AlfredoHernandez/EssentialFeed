07de262c9229c603284229dfd5b40f1cf5229914
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-09-02 13:00:06 -0500

    Moves `FeedViewControllerTests` to its own files

--- EssentialFeediOS/Feed UI/Views/UIImageView+Animations.swift
+++ EssentialFeediOS/Feed UI/Views/UIImageView+Animations.swift
@@ -6,9 +6,9 @@ import UIKit
 extension UIImageView {
     func setImageAnimated(_ newImage: UIImage?) {
         image = newImage
-        
+
         guard newImage != nil else { return }
-        
+
         alpha = 0
         UIView.animate(withDuration: 0.25) {
             self.alpha = 1
---
+++ EssentialFeediOSTests/Feed UI/Controllers/Helpers/FeedImageCell+TestHelpers.swift
@@ -0,0 +1,36 @@
+//
+//  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import EssentialFeediOS
+import UIKit
+
+extension FeedImageCell {
+    var isShowingLocation: Bool {
+        !locationContainer.isHidden
+    }
+
+    var locationText: String? {
+        locationLabel.text
+    }
+
+    var descriptionText: String? {
+        descriptionLabel.text
+    }
+
+    var isShowingImageLoadingIndicator: Bool {
+        feedImageContainer.isShimmering
+    }
+
+    var renderedImage: Data? {
+        feedImageView.image?.pngData()
+    }
+
+    var isShowingRetryAction: Bool {
+        !feedImageRetryButton.isHidden
+    }
+
+    func simulateRetryAction() {
+        feedImageRetryButton.simulateTap()
+    }
+}
