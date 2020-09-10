d94243968184085c010abaafff84761b9059f6d4
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-09-10 15:56:57 -0500

    Move `FeedViewModel` to separate file

--- EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -8,10 +8,6 @@ public protocol FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel)
 }
 
-public struct FeedViewModel {
-    public let feed: [FeedImage]
-}
-
 public protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
