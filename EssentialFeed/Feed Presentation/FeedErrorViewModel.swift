7973702a05cc2e082a48f15bf88718c7607109a0
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-09-10 15:49:22 -0500

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import Foundation
+
+public struct FeedErrorViewModel {
+    public let message: String?
+
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}
--- EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -16,18 +16,6 @@ public protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
 
-public struct FeedErrorViewModel {
-    public let message: String?
-
-    static var noError: FeedErrorViewModel {
-        return FeedErrorViewModel(message: nil)
-    }
-    
-    static func error(message: String) -> FeedErrorViewModel {
-        return FeedErrorViewModel(message: message)
-    }
-}
-
 public protocol FeedErrorView {
     func display(_ viewModel: FeedErrorViewModel)
 }
