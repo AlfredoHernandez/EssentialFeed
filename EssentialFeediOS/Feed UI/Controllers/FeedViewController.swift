d82488a5840116ade8b36445cc2911a262f56c6f
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-09-10 14:17:04 -0500

    Extract refreshing logic to an extension

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -60,10 +60,6 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
 
     func display(_ viewModel: FeedLoadingViewModel) {
-        if viewModel.isLoading {
-            refreshControl?.beginRefreshing()
-        } else {
-            refreshControl?.endRefreshing()
-        }
+        refreshControl?.update(isRefreshing: viewModel.isLoading)
     }
 }
---
+++ EssentialFeediOS/Feed UI/Views/Helpers/UIRefreshControl+Helpers.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import UIKit
+
+extension UIRefreshControl {
+   func update(isRefreshing: Bool) {
+       isRefreshing ? beginRefreshing() : endRefreshing()
+   }
+}
