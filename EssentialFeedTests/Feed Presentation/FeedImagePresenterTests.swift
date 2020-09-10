9686e3c125d58e461a4bd89269d2bb23617edcd1
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-09-10 17:03:26 -0500

    `FeedImagePresenter` displays retry on `didFinishLoadingImageDataWithError`

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -51,6 +51,16 @@ class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
             shouldRetry: image == nil
         ))
     }
+    
+    func didFinishLoadingImageData(with _: Error, for model: FeedImage) {
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: false,
+            shouldRetry: true
+        ))
+    }
 }
 
 class FeedImagePresenterTests: XCTestCase {
@@ -108,6 +118,22 @@ class FeedImagePresenterTests: XCTestCase {
         XCTAssertEqual(message?.isLoading, false)
         XCTAssertEqual(message?.shouldRetry, false)
         XCTAssertEqual(message?.image, transformedData)
+    }
+    
+    
+    func test_didFinishLoadingImageDataWithError_displaysRetry() {
+        let image = uniqueImage()
+        let (sut, view) = makeSUT()
+        
+        sut.didFinishLoadingImageData(with: anyNSError(), for: image)
+        
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.description, image.description)
+        XCTAssertEqual(message?.location, image.location)
+        XCTAssertEqual(message?.isLoading, false)
+        XCTAssertEqual(message?.shouldRetry, true)
+        XCTAssertNil(message?.image)
     }
     
     // MARK: - Helpers
