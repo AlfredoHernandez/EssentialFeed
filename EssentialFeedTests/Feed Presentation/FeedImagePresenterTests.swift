d24b74c4392fd82b759144679406048acc89c35f
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-09-10 17:04:20 -0500

    Inline `Data` creation

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -90,9 +90,8 @@ class FeedImagePresenterTests: XCTestCase {
         let (sut, view) = makeSUT(imageTransformer: fail)
         
         let image = uniqueImage()
-        let data = Data()
         
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with: Data(), for: image)
         
         let message = view.messages.first
         XCTAssertEqual(view.messages.count, 1)
@@ -105,11 +104,10 @@ class FeedImagePresenterTests: XCTestCase {
 
     func test_didFinishLoadingImageData_displaysImageOnSuccessfulTransformation() {
         let image = uniqueImage()
-        let data = Data()
         let transformedData = AnyImage()
         let (sut, view) = makeSUT(imageTransformer: { _ in transformedData })
 
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with: Data(), for: image)
 
         let message = view.messages.first
         XCTAssertEqual(view.messages.count, 1)
