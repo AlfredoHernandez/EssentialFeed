b68c87e94e184a8aff9e86e05873323501bef00c
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-09-13 14:00:59 -0500

    Add `RemoteFeedImageDataLoader` end-to-end test to guarantee both the client (app) and server (backend) respects the API contract, so we can fetch image data from remote.

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -24,6 +24,19 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
             XCTFail("Expected successfyll feed result, got no result instead.")
         }
     }
+    
+    func test_endToEndTestServerGETFeedImageDataResult_matchesFixedTestAccountData() {
+         switch getFeedImageDataResult() {
+         case let .success(data)?:
+             XCTAssertFalse(data.isEmpty, "Expected non-empty image data")
+
+         case let .failure(error)?:
+             XCTFail("Expected successful image data result, got \(error) instead")
+
+         default:
+             XCTFail("Expected successful image data result, got no result instead")
+         }
+     }
 
     // MARK: - Test helpers
 
@@ -45,6 +58,25 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
         wait(for: [exp], timeout: 5.0)
         return receivedResult
     }
+    
+    private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
+         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")!
+         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+         let loader = RemoteFeedImageDataLoader(client: client)
+         trackForMemoryLeaks(client, file: file, line: line)
+         trackForMemoryLeaks(loader, file: file, line: line)
+
+         let exp = expectation(description: "Wait for load completion")
+
+         var receivedResult: FeedImageDataLoader.Result?
+         _ = loader.loadImageData(from: testServerURL) { result in
+             receivedResult = result
+             exp.fulfill()
+         }
+         wait(for: [exp], timeout: 5.0)
+
+         return receivedResult
+     }
 
     private func expectedImage(at index: Int) -> FeedImage {
         return FeedImage(
