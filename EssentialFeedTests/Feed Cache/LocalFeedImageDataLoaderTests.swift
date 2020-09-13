1cde5e544532d0b727cea889cab07a7706ba0a75
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-09-13 15:21:43 -0500

    Extracts SUT creation into a helper method

--- EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -5,18 +5,26 @@
 import XCTest
 
 class LocalFeedImageDataLoader {
-    init(store: Any) { }
+    init(store _: Any) {}
 }
 
 class LocalFeedImageDataLoaderTests: XCTestCase {
-    
     func test_init_doesNotMessageStoreUponCreation() {
-        let store = FeedStoreSpy()
-        _ = LocalFeedImageDataLoader(store: store)
-        
+        let (_, store) = makeSUT()
+
         XCTAssertTrue(store.messages.isEmpty)
     }
-    
+
+    // MARK: - Helpers
+
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (LocalFeedImageDataLoader, FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedImageDataLoader(store: store)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(store, file: file, line: line)
+        return (sut, store)
+    }
+
     private class FeedStoreSpy {
         var messages = [Any]()
     }
