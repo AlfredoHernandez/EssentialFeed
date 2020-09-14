81690397f3fffb3cab40debdbe694c90896d1f25
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-09-14 17:23:00 -0500

    `LocalFeedImageDataLoader.saveImageDataFromURL` does not deliver result after instance has been deallocated

--- EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -20,7 +20,8 @@ extension LocalFeedImageDataLoader {
     }
 
     public func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
-        store.insert(data, for: url) { result in
+        store.insert(data, for: url) { [weak self] result in
+            guard self != nil else { return }
             switch result {
             case .success:
                 completion(.success(()))
--- EssentialFeedTests/Feed Cache/CacheFeedImageDataUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedImageDataUseCaseTests.swift
@@ -38,6 +38,18 @@ class CacheFeedImageDataUseCaseTests: XCTestCase {
         })
     }
 
+    func test_saveImageDataFromURL_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+        let store = FeedImageDataStoreSpy()
+        var sut: LocalFeedImageDataLoader? = LocalFeedImageDataLoader(store: store)
+
+        var receivedResults = [FeedImageDataStore.InsertionResult]()
+        sut?.save(anyData(), for: anyURL()) { receivedResults.append($0) }
+        sut = nil
+
+        store.completeInsertionSuccessfully()
+        XCTAssertTrue(receivedResults.isEmpty, "Expected no received results after instance has been deallocated")
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedImageDataStoreSpy) {
