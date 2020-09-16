e3f066e88e09e3d6c786f8ef0265cd4fab080f7a
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-09-15 20:02:50 -0500

    Create EssentialApp workspace - Contains EssentialApp empty project and EssentialFeed project

---
+++ EssentialApp/EssentialAppTests/EssentialAppTests.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+@testable import EssentialApp
+import XCTest
+
+class EssentialAppTests: XCTestCase {
+    func test_is_true() {
+        XCTAssertTrue(true)
+    }
+}
