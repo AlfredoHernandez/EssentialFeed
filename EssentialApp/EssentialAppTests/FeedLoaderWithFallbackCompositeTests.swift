62bc65017e400fb4e790b9ecb86dcfab7f8a462d
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-09-23 15:24:09 -0500

    `FeedLoaderWithFallbackComposite.load` delivers fallback feed on primary loader failure

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -16,7 +16,14 @@ class FeedLoaderWithFallbackComposite: FeedLoader {
     }
 
     func load(completion: @escaping (Result<[FeedImage], Error>) -> Void) {
-        primary.load(completion: completion)
+        primary.load { [self] result in
+            switch result {
+            case .success:
+                completion(result)
+            case .failure:
+                fallback.load(completion: completion)
+            }
+        }
     }
 }
 
@@ -25,12 +32,29 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         let primaryFeed = uniqueFeed()
         let fallbackFeed = uniqueFeed()
         let sut = makeSUT(primaryResult: .success(primaryFeed), fallbackResult: .success(fallbackFeed))
-        
+
         let exp = expectation(description: "Wait for load completion")
         sut.load { result in
             switch result {
             case let .success(receivedFeed):
                 XCTAssertEqual(receivedFeed, primaryFeed)
+            case .failure:
+                XCTFail("Expected successful load feed result, but got \(result) instead.")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+
+    func test_load_deliversFallbackFeedOnPrimaryFailure() {
+        let fallbackFeed = uniqueFeed()
+        let sut = makeSUT(primaryResult: .failure(NSError(domain: "any error", code: 1)), fallbackResult: .success(fallbackFeed))
+
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case let .success(receivedFeed):
+                XCTAssertEqual(receivedFeed, fallbackFeed)
             case .failure:
                 XCTFail("Expected successful load feed result, but got \(result) instead.")
             }
