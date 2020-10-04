79dd4c6ded08421d8baac796b751a78e613abd69
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-10-04 15:19:46 -0500

    Reformats code

--- EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -1,25 +1,21 @@
 //
-//  FeedSnapshotTests.swift
-//  EssentialFeediOSTests
-//
-//  Created by Jesús Alfredo Hernández Alarcón on 04/10/20.
 //  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
 //
 
-import XCTest
 import EssentialFeediOS
+import XCTest
 
 class FeedSnapshotTests: XCTestCase {
     func test_emptyFeed() {
         let sut = makeSUT()
-        
+
         sut.display(emptyFeed())
-        
+
         record(snapshot: sut.snapshot(), named: "empty_feed")
     }
-    
+
     // MARK: - Helpers
-    
+
     func makeSUT() -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
@@ -27,22 +23,22 @@ class FeedSnapshotTests: XCTestCase {
         controller.loadViewIfNeeded()
         return controller
     }
-    
+
     private func emptyFeed() -> [FeedImageCellController] {
-        return []
+        []
     }
-    
+
     private func record(snapshot: UIImage, named: String, file: StaticString = #file, line: UInt = #line) {
         guard let snapshotData = snapshot.pngData() else {
             XCTFail("Failed to generate PNG data representation for snapshot", file: file, line: line)
             return
         }
-        
+
         let snapshotURL = URL(fileURLWithPath: String(describing: file))
             .deletingLastPathComponent()
             .appendingPathComponent("snapshots")
             .appendingPathComponent("\(named).png")
-        
+
         do {
             try FileManager.default.createDirectory(at: snapshotURL.deletingLastPathComponent(), withIntermediateDirectories: true)
             try snapshotData.write(to: snapshotURL)
