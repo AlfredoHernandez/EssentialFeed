0f62ac47e7dfb883e18b14d458c3b82f736f24dc
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-11-08 16:30:01 -0600

    Delivers proper results on 2xx response

--- EssentialFeed/Feed API/ImageCommentsMapper.swift
+++ EssentialFeed/Feed API/ImageCommentsMapper.swift
@@ -12,10 +12,14 @@ internal final class ImageCommentsMapper {
     }
 
     internal static func map(data: Data, response: HTTPURLResponse) throws -> [RemoteFeedItem] {
-        guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data)
+        guard isOk(response), let root = try? JSONDecoder().decode(Root.self, from: data)
         else {
             throw RemoteImageCommentsLoader.Error.invalidData
         }
         return root.items
+    }
+
+    private static func isOk(_ response: HTTPURLResponse) -> Bool {
+        (200 ... 299).contains(response.statusCode)
     }
 }
