a2452c48a0db92cbaffbc61a82e69fdf9f0741c5
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-11-08 18:27:08 -0600

    Replace `RemoteFeedLoader` with generic loader

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -4,40 +4,10 @@
 
 import Foundation
 
-public final class RemoteFeedLoader: FeedLoader {
-    private let url: URL
-    private let client: HTTPClient
+public typealias RemoteFeedLoader = RemoteLoader<[FeedImage]>
 
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-
-    public typealias Result = FeedLoader.Result
-
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-
-    public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            switch result {
-            case let .success((data, response)):
-                completion(RemoteFeedLoader.map(data, from: response))
-            case .failure:
-                completion(.failure(Error.connectivity))
-            }
-        }
-    }
-
-    private static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-        do {
-            let items = try FeedItemMapper.map(data: data, response: response)
-            return .success(items)
-        } catch {
-            return .failure(error)
-        }
+public extension RemoteFeedLoader {
+    convenience init(url: URL, client: HTTPClient) {
+        self.init(url: url, client: client, mapper: FeedItemMapper.map)
     }
 }
--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -6,40 +6,6 @@ import EssentialFeed
 import XCTest
 
 class LoadFeedFromRemoteUseCaseTests: XCTestCase {
-    func test_init_doesNotRequestDataFromURL() {
-        let (_, client) = makeSUT()
-
-        XCTAssertTrue(client.requestedURLs.isEmpty)
-    }
-
-    func test_init_requestsDataFromURL() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-
-        sut.load { _ in }
-
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
-
-    func test_initTwice_requestsDataFromURLTwice() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-
-        sut.load { _ in }
-        sut.load { _ in }
-
-        XCTAssertEqual(client.requestedURLs, [url, url])
-    }
-
-    func test_load_deliversErrorOnClientError() {
-        let (sut, client) = makeSUT()
-
-        expect(sut, toCompleteWith: failure(.connectivity), when: {
-            let clientError = NSError(domain: "Test", code: 0)
-            client.complete(with: clientError)
-        })
-    }
-
     func test_load_deliversErrorOnNon200HTTPResonse() {
         let (sut, client) = makeSUT()
 
@@ -91,19 +57,6 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
             let json = makeItemsJSON([item1.json, item2.json])
             client.complete(withStatusCode: 200, data: json)
         })
-    }
-
-    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let url = URL(string: "http://any-url.com")!
-        let client = HTTPClientSpy()
-        var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
-
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut?.load { capturedResults.append($0) }
-        sut = nil
-        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
-
-        XCTAssertTrue(capturedResults.isEmpty)
     }
 
     // MARK: Tests helpers
