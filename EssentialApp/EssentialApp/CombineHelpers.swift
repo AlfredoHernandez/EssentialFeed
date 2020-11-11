20de7c94a31d629d0ab827a9defc02693e364a79
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-11-10 19:33:56 -0600

    FeedLoader with Combine integration * Replace `RemoteLoader` composition with `HTTPClient` publisher composed with `FeedItemsMapper`

--- EssentialApp/EssentialApp/CombineHelpers.swift
+++ EssentialApp/EssentialApp/CombineHelpers.swift
@@ -6,6 +6,22 @@ import Combine
 import EssentialFeed
 import Foundation
 
+public extension HTTPClient {
+    typealias Publisher = AnyPublisher<(Data, HTTPURLResponse), Error>
+
+    func getPublisher(from url: URL) -> Publisher {
+        var task: HTTPClientTask?
+
+        return Deferred {
+            Future { completion in
+                task = self.get(from: url, completion: completion)
+            }
+        }
+        .handleEvents(receiveCancel: { task?.cancel() })
+        .eraseToAnyPublisher()
+    }
+}
+
 public extension FeedImageDataLoader {
     typealias Publisher = AnyPublisher<Data, Swift.Error>
 
--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -43,9 +43,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             URL(
                 string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json"
             )!
-        let remoteFeedLoader = RemoteLoader(url: remoteURL, client: httpClient, mapper: FeedItemMapper.map)
-        let loaderAsPublisher = remoteFeedLoader
-            .loadPublisher()
+
+        let loaderAsPublisher = httpClient
+            .getPublisher(from: remoteURL)
+            .tryMap(FeedItemMapper.map)
             .catching(to: localFeedLoader)
             .fallback(to: localFeedLoader.loadPublisher)
 
