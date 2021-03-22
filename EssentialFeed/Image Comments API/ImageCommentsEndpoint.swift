003e4013e381197e55daee21fdc29e00768934c9
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-03-21 19:17:22 -0600

    Extract endpoint URL creation to the API modules

---
+++ EssentialFeed/Image Comments API/ImageCommentsEndpoint.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright © 2021 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import Foundation
+
+public enum ImageCommentsEndpoint {
+    case get(UUID)
+
+    public func url(baseURL: URL) -> URL {
+        switch self {
+        case let .get(id):
+            return baseURL.appendingPathComponent("/v1/image/\(id)/comments")
+        }
+    }
+}
---
+++ EssentialFeedTests/Image Comments API/ImageCommentsEndpointTests.swift
@@ -0,0 +1,18 @@
+//
+//  Copyright © 2021 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import EssentialFeed
+import XCTest
+
+class ImageCommentsEndpointTests: XCTestCase {
+    func test_imageComments_endpointURL() {
+        let imageID = UUID(uuidString: "2239CBA2-CB35-4392-ADC0-24A37D38E010")!
+        let baseURL = URL(string: "http://base-url.com")!
+
+        let received = ImageCommentsEndpoint.get(imageID).url(baseURL: baseURL)
+        let expected = URL(string: "http://base-url.com/v1/image/2239CBA2-CB35-4392-ADC0-24A37D38E010/comments")!
+
+        XCTAssertEqual(received, expected)
+    }
+}
